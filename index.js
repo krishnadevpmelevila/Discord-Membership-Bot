c38ee382a4d1ad3d8fbcfa4e5f00180d66127abe
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2021-11-14 13:47:48 +0530

    Bot updated

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.on('messageCreate', (message) => {
                 message.delete();
                 message.member.roles.add('908010320675635200');
                 message.member.roles.remove('908013072428445766');
-                message.guild.channels.cache.get('909278509552267274').send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
+                message.author.send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
 
             }
             

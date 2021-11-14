d5aa1bf2959acf1c2a0e2d4e84aa5680b350a4f2
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2021-11-14 15:15:58 +0530

    Requirements updated

--- test.js
+++ test.js
@@ -1,36 +1,3 @@
-// const axios = require('axios');
+axios = require('axios');
+require('dotenv').config();
 
-// require('dotenv').config();
-// axios.post(process.env.API_URL + '/login', {
-
-
-//     email: process.env.USER_NAME,
-//     password: process.env.PASS
-
-
-// }).then(function (response) {
-//     axios.get(process.env.API_URL+'/data', {
-//         headers: {
-//             'x-access-token': response.data.token
-//         }
-//     }).then(function (response) {
-//         var data = response.data
-//         for (var index = 0; index < data.length; ++index) {
-
-//             var user = data[index];
-//             if (user.acti==message.) {
-//                 message.delete();
-//                 message.member.roles.add('908010320675635200');
-//                 message.member.roles.remove('908013072428445766');
-//                 message.author.send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
-
-//             }
-//         }
-//     })
-    
-//         // console.log(response.data);
-        
-
-    
-
-// })

8e84e30937d52f416cafea026588092e9a1c6bce
krishnadevpmelevila <krishnadevpmelevila@gmail.com>
2021-11-14 14:14:36 +0530

    Bot updated

--- index.js
+++ index.js
@@ -41,13 +41,13 @@ bot.on('messageCreate', (message) => {
 
 
         }).then(function (response) {
-            if (response.data.activated != true) {
-                message.delete();
-                message.member.roles.add('908010320675635200');
-                message.member.roles.remove('908013072428445766');
-                message.author.send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
+            // if (response.data.activated != true) {
+            //     message.delete();
+            //     message.member.roles.add('908010320675635200');
+            //     message.member.roles.remove('908013072428445766');
+            //     message.author.send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
 
-            }
+            // }
             
         })
     }
